435d74fbc28d356239909aa7a3317a9271580fd5
phil <phil@branch14.org>
2018-12-15 21:40:36 +0100

    templating mostly

--- src/easy/core.cljs
+++ src/easy/core.cljs
@@ -8,10 +8,11 @@
 
    ;; via npm
    ["js-yaml" :as yaml]
+   ["handlebars" :as hbs]
 
    ;; via clojars/maven
+   [cljs-time.core :as cljs-time]
    [cljs-time.format :as time]
-   ;;[basil.public :as basil]
 
    ;; from this codebase
    [easy.item :as item]
@@ -19,7 +20,8 @@
 
    ;; from clojurescript stdlib
    [cljs.pprint :refer [pprint]]
-   [cljs.spec.alpha :as s]))
+   [cljs.spec.alpha :as s]
+   [clojure.string :refer [join]]))
 
 (defn slurp [path]
   (.readFileSync fs path "utf8"))
@@ -27,6 +29,16 @@
 (defn parse-yaml [string]
   (-> (yaml/load string)
       (js->clj :keywordize-keys true)))
+
+(def iso-formatter
+  (time/formatter "yyyy-MM-dd"))
+
+(defn add-isodate [event]
+  (->> event
+       :date
+       cljs-time/date-time
+       (time/unparse iso-formatter)
+       (assoc event :isodate)))
 
 (defn tax-rate-in [revenue]
   (if (< (:date revenue)
@@ -89,14 +101,25 @@
           (:tax-in revenue))
        (assoc revenue :gross-total)))
 
+(defn add-invoice-no [revenue]
+  (->> [:customer :number :version]
+       (map revenue)
+       (join ".")
+       (assoc revenue :invoice-no)))
+
+(defn add-ledger-template [revenue]
+  (assoc revenue :ledger-template "vorlagen/ledger.dat.hbs"))
+
 (defmulti transform
   "Events will be transformed based on their type."
   (comp keyword :type))
 
 (defmethod transform :revenue [event]
   (-> event
+      add-isodate
       add-tax-rate-in
       add-tax-rate-out
+      ;; TODO add-tax-period
       ;; TODO read-timesheets
       ;; TODO add-items-hours
       add-items-amount
@@ -104,11 +127,22 @@
       add-tax-in
       add-tax-out
       add-tax-win
-      add-gross-total))
+      add-gross-total
+      add-invoice-no
+      add-ledger-template))
 
 (defmethod transform :expense [event]
   ;; TODO implement
   event)
+
+(defn apply-template [template-key event]
+  (let [path (template-key event)
+        source (slurp path)
+        renderer (hbs/compile source)]
+    (renderer (clj->js event))))
+
+(def render-ledger
+  (partial apply-template :ledger-template))
 
 (defn -main [& args]
   (let [content (slurp (first args))
@@ -116,10 +150,13 @@
     (if (s/valid? ::event/events events)
       (let [;; TODO item/merge-defaults
             input-with-defaults (map event/merge-defaults events)
-            output (map transform input-with-defaults)]
-        (println "IN")
-        (pprint input-with-defaults)
-        (println "OUT")
-        (pprint output))
+            output (map transform input-with-defaults)
+            ledger (map render-ledger output)]
+        ;; (println "INPUT EVENTS")
+        ;; (pprint input-with-defaults)
+        ;; (println "TRANSFORMED EVENTS")
+        ;; (pprint output)
+        ;; (println "LEDGER")
+        (println (join "\n" ledger)))
       ;; if invalid explain...
       (s/explain ::event/events events))))
